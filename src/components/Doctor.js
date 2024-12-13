db942a33e04fe45441ae9b107c09d9f38115be73
abolfazl <kylink17estiri@gmail.com>
2024-12-13 16:13:41 +0330

    update name

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import './Doctor.css'
 import { Container } from 'react-bootstrap';
-import CardDoctor from './CardDoctor';
+import Doctorcard from './Doctorcard';
 
 
 export default function Doctor() {
   return (
         <Container>
             <div className='doctor-card mt-3'>
-                <CardDoctor img='./img/littleimg-1.png' name='زهرا حیدری' des='کارشناس مراقبت های پوستی' />
-                <CardDoctor img='./img/littleimg-2.png' name='روژینا موسوی' des='تراپیست ، نروتراپیست' />
-                <CardDoctor img='./img/littleimg-3.png' name='علی جعفری' des='متخصص گوارش' />
-                <CardDoctor img='./img/littleimg-4.png' name='فریما عبدلاحی' des='متخصص دندانپزشکی' />
+                <Doctorcard img='./img/littleimg-1.png' name='زهرا حیدری' des='کارشناس مراقبت های پوستی' />
+                <Doctorcard img='./img/littleimg-2.png' name='روژینا موسوی' des='تراپیست ، نروتراپیست' />
+                <Doctorcard img='./img/littleimg-3.png' name='علی جعفری' des='متخصص گوارش' />
+                <Doctorcard img='./img/littleimg-4.png' name='فریما عبدلاحی' des='متخصص دندانپزشکی' />
             </div >
         </Container>
   );
