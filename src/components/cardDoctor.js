3ec19eda831334b52a9c53cc99f7954bd7153f05
abolfazl <kylink17estiri@gmail.com>
2024-12-13 15:50:53 +0330

    doctor card name changed

--- src/components/cardDoctor.js
+++ src/components/cardDoctor.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 
-export default function cardDoctor(props) {
+export default function CardDoctor (props) {
   return (
     <>
         <div className='card-doctor'>
