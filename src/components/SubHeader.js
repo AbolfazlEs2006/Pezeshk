7d3d0660eca48f6f8056d52c1e576fbd9e3d84b6
abolfazl <kylin17estiri@gmail.ciom>
2025-02-26 14:36:51 +0330

    update responsive design

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import { Navbar , Container } from 'react-bootstrap'
-import './SubHeader.css'
-import Swipers from "./Swipers"
-
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+import "./SubHeader.css";
+import Swipers from "./Swipers";
 
 export default function SubHeader() {
   return (
     <>
       <Navbar expand="lg d-flex">
-        <Container className='sub-container'>
-          <Navbar.Brand href="/" className='Nav-logo me-0'><img src='./img/article.png' /></Navbar.Brand>
+        <Container className="sub-container">
+          <Navbar.Brand href="/" className="Nav-logo me-0">
+            <img src="./img/article.png" />
+          </Navbar.Brand>
           <Swipers />
-          <input className='sub-search' placeholder='جستجوی سرویس مورد نظرتان...' type='search'></input>
+          <input
+            className="sub-search"
+            placeholder="جستجوی سرویس مورد نظرتان..."
+            type="search"
+          ></input>
         </Container>
-    </Navbar>
+      </Navbar>
     </>
-  )
+  );
 }
